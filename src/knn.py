5a0224a2ee71c63ac54d1004d350cf9fbe07bf69
Randy Zakya <impper1@gmail.com>
2021-06-30 15:04:08 +0700

    knn and logreg faster loop

--- src/knn.py
+++ src/knn.py
@@ -1,50 +1,49 @@
 #KNN
 import math
-from statistics import mode
+import pandas as pd
+import time
 
 class ColumnNumberError(Exception):
 	pass
 
 class KNNClassifier:
-	def __init__(self,df,category_column):
-		self.df = df
-		self.category_column = category_column
-		self.categorical_columns = []
-		start = 0 if df.columns == None else 1
-		for i in range(start,len(df)):
-			for j in range(len(df[i])):
-				try:
-					val = float(df[i][j])
-					df[i][j] = val
-				except:
-					if j not in self.categorical_columns:
-						self.categorical_columns.append(j)
+	def __init__(self,df,x_labels, y_label):
+		self.df = df.copy()
+		self.x_labels = [self.df.columns[i] for i in x_labels]
+		self.y_label = self.df.columns[y_label]
 
 	def euclidean_distance(self,data,other):
-		deltas = []
-		for i in range(len(data)):
-			if i not in self.categorical_columns and i != self.category_column:
-				deltas.append(data[i]-other[i])
-		total_squared = 0
-		for j in deltas:
-			total_squared += j**2
-		return math.sqrt(total_squared)
+		total = 0
+		for label in self.x_labels:
+			delta = (data[label] - other[label])
+			total += delta**2
+		return math.sqrt(total)
 
+	#using apply function to reduce time taken
 	def predict(self,data,k):
-		if len(data) != len(self.df[0])-1:
+		start = time.time()
+		if len(data) != len(self.x_labels):
 			raise ColumnNumberError()
+		temp = data
+		data = {}
+		for i in range(len(self.x_labels)):
+			data[self.x_labels[i]] = temp[i]
 
-		sorted_df = self.df[1:] if self.df.columns != None else self.df[:]
-		for i in range(len(sorted_df)):
-			nearest = sorted_df[i]
-			for j in range(i+1,len(sorted_df)):
-				if (self.euclidean_distance(data,sorted_df[i]) > self.euclidean_distance(data,sorted_df[j])):
-					nearest = sorted_df[j]
-			sorted_df[j] = sorted_df[i]
-			sorted_df[i] = nearest
-
-		categories = []
-		offset = 1 if self.df.columns != None else 0
-		for num in range(offset,k+offset):
-			categories.append(sorted_df[num][self.category_column])
-		return mode(categories)
+		# sorted_df = self.df.copy()
+		# for irow in range(len(sorted_df)):
+		# 	nearest = irow
+		# 	for jrow in range(irow+1,len(sorted_df)):
+		# 		if self.euclidean_distance(data,sorted_df.iloc[jrow]) < self.euclidean_distance(data,sorted_df.iloc[nearest]):
+		# 			nearest = jrow
+		# 	temp = sorted_df.iloc[irow].copy()
+		# 	sorted_df.iloc[irow] = sorted_df.iloc[nearest]
+		# 	sorted_df.iloc[nearest] = temp
+		self.df['distance'] = self.df.apply(lambda row: self.euclidean_distance(data,row[self.x_labels]),axis = 1)
+		# categories = []
+		# for irow in range(k):
+		# 	category = sorted_df.iloc[irow][self.y_label]
+		# 	categories.append(category)
+		result = self.df.nsmallest(k,columns = 'distance')[self.y_label].mode()[0]
+		time_taken = round(time.time() - start, 4)
+		print("Time taken to predict : " + str(time_taken) + " s")
+		return result
--- src/logreg.py
+++ src/logreg.py
@@ -1,5 +1,8 @@
 #logistic
 import math
+import pandas as pd
+import numpy as np
+import time
 
 def sigmoid(x):
 	try:
@@ -25,55 +28,45 @@ def dot(X1,X2):
 	return res
 
 class LogisticRegression:
-	def __init__(self,df):
-		self.df = df
-		for i in range(len(self.df)):
-			for j in range(len(self.df[i])):
-				try:
-					val = float(self.df[i][j])
-					self.df[i][j] = val
-				except:
-					pass
+	def __init__(self):
 		self.b_values = None
-		self.y_vals= None
+		self.y_vals = None
 
-	#x_columns bentuknya list of index column yg jadi x, y cmn int
-	def train(self,x_columns, y_column, alpha, epochs):
-		offset = 1 if self.df.columns != None else 0
-		self.y_vals = []
-		for i in range(offset,len(self.df)):
-			val = self.df[i][y_column]
-			if val not in self.y_vals:
-				self.y_vals.append(val)
+	def train(self,df_,x_labels, y_label, alpha, epochs):
+		df = df_.copy()
+		x_labels = [df.columns[i] for i in x_labels]
+		y_label = df.columns[y_label]
+		self.y_vals = df[y_label].unique()
 		if len(self.y_vals) != 2:
 			raise NotBinaryClassificationProblemError()
 		else:
 			if 1 not in self.y_vals or 0 not in self.y_vals:
-				for irow in range(offset,len(self.df)):
-					self.df[irow][y_column] = 0 if self.df[irow][y_column] == self.y_vals[0] else 1
+				df.loc[df[y_label] == self.y_vals[0], y_label] = 0
+				df.loc[df[y_label] == self.y_vals[1], y_label] = 1
 
-		b_values = [0 for i in range(len(x_columns) + 1)]
+		b_values = [0 for i in range(1 + len(x_labels))]
 
 		for epoch in range(epochs):
-			for irow in range(offset,len(self.df)):
-				row_vector = [1] + [self.df[irow][j] for j in x_columns]
+			start = time.time()
+			#using iterrows to reduce loop time
+			for idx, row in df.iterrows():
+				row_vector = [1] + [row[j] for j in x_labels]
 				z = dot(row_vector,b_values)
 				prediction = sigmoid(z)
-				for ib in range(len(b_values)):
-					b_values[ib] += alpha*(self.df[irow][y_column]-prediction)*prediction*(1-prediction)*row_vector[ib]
+				b_values = [b_values[i] + alpha*(row[y_label] - prediction)*prediction*(1 - prediction)*row_vector[i] for i in range(len(b_values))]
 
 			naccurate = 0
-			for irow in range(offset, len(self.df)):
-				row_vector = [1] + [self.df[irow][j] for j in x_columns]
+			for idx,row in df.iterrows():
+				row_vector = [1] + [row[j] for j in x_labels]
 				z = dot(row_vector,b_values)
 				prediction = sigmoid(z)
-				if prediction < 0.5 and self.df[irow][y_column] == 0:
+				if prediction < 0.5 and row[y_label] == 0:
 					naccurate += 1
-
-				if prediction >= 0.5 and self.df[irow][y_column] == 1:
+				if prediction >= 0.5 and row[y_label] == 1:
 					naccurate += 1
-			accuracy = naccurate/(len(self.df) - offset)
-			print("Epoch : " + str(epoch + 1) + " | Accuracy : " + str(accuracy))
+			accuracy = naccurate/len(df)
+			time_taken = round(time.time() - start,4)
+			print("Epoch : " + str(epoch + 1) + " | Accuracy : " + str(accuracy) + " | Time : " + str(time_taken) + " s")
 		self.b_values = b_values
 
 	def predict(self,data):
